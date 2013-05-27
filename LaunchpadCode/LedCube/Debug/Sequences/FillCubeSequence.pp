b093ff8a2153f55a2678e2bd5dde5c1d30e666d2
RobertCL <RobertCL@iname.com>
2013-05-27 12:47:44 +0100

    Remove the Debug folder.  Should never have committed it in the first place!

--- LaunchpadCode/LedCube/Debug/Sequences/FillCubeSequence.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-Sequences/FillCubeSequence.obj: ../Sequences/FillCubeSequence.cpp
-Sequences/FillCubeSequence.obj: ../Sequences/FillCubeSequence.h
-Sequences/FillCubeSequence.obj: ../Sequences/Sequence.h
-
-../Sequences/FillCubeSequence.cpp: 
-../Sequences/FillCubeSequence.h: 
-../Sequences/Sequence.h: 
--- LaunchpadCode/LedCube/Debug/Sequences/Sequence.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-Sequences/Sequence.obj: ../Sequences/Sequence.cpp
-Sequences/Sequence.obj: ../Sequences/Sequence.h
-
-../Sequences/Sequence.cpp: 
-../Sequences/Sequence.h: 
