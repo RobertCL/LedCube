8ac1886707353369d5687492df89703d5f23e95d
RobertCL <RobertCL@iname.com>
2014-10-11 11:24:40 +0100

    RandomFillCubeSequence tweaks

--- EnergiaCode/LedCube/RandomFillCubeSequence.cpp
+++ EnergiaCode/LedCube/RandomFillCubeSequence.cpp
@@ -7,12 +7,18 @@ void RandomFillCubeSequence::initialize(){
   Sequence::initialize();
   delay = 1;
   runCycles = 512;
-  name = "FillCube";
+  name = "RandomFillCube";
 }
+
+int cycle = 0;
 
 void RandomFillCubeSequence::next(){
 
-  for(int i=0; i<100 ;i++) {
+  cycle++;
+  
+  // Seems to need a way to abort because in some cases no available
+  // random position can be found, even when the cube isn't full.
+  for(int i=0; i<150 ;i++) {
     int x = rand() % 8;
     int y = rand() % 8;
     int z = rand() % 8;
@@ -22,5 +28,7 @@ void RandomFillCubeSequence::next(){
       return;
     }
   }
-  Serial.println("No random position found");
+  
+  Serial.print("No random position found on cycle ");
+  Serial.println(cycle);
 }
